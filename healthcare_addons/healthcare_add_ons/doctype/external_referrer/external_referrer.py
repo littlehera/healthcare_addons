4527233d39db4f47aeae7ff50fba3b0364d8fdc7
Herabelle Villanueva <villanuevahera@gmail.com>
2023-09-21 14:36:08 +0800

    removed external referrer, updated fixtures

--- healthcare_addons/healthcare_add_ons/doctype/external_referrer/external_referrer.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, littlehera and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class ExternalReferrer(Document):
-	pass
