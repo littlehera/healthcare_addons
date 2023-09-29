df9c6d37587d7a51eab7307f0c75acb0d584b49a
Herabelle Villanueva <villanuevahera@gmail.com>
2023-09-29 11:42:59 +0800

    Summary of PF report

--- healthcare_addons/healthcare_add_ons/report/summary_of_professional_fees/summary_of_professional_fees.js
+++ healthcare_addons/healthcare_add_ons/report/summary_of_professional_fees/summary_of_professional_fees.js
@@ -4,6 +4,31 @@
 
 frappe.query_reports["Summary of Professional Fees"] = {
 	"filters": [
-
+		{
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "reqd": 1
+        },
+		{
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "reqd": 1
+        },
+		{
+			"fieldname":"report_type",
+			"label": __("Report Type"),
+			"fieldtype": "Select",
+            "options": ['MD Consultation PF','Reading PF'],
+            "reqd": 1
+		},
+		{
+            "fieldname": "ref_practitioner",
+            "label": __("Doctor"),
+            "fieldtype": "Link",
+            "options": "Healthcare Practitioner",
+            "reqd": 0
+        }
 	]
 };
--- healthcare_addons/healthcare_add_ons/report/summary_of_professional_fees/summary_of_professional_fees.py
+++ healthcare_addons/healthcare_add_ons/report/summary_of_professional_fees/summary_of_professional_fees.py
@@ -1,9 +1,31 @@
 # Copyright (c) 2023, littlehera and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 
 
 def execute(filters=None):
-	columns, data = [], []
+	from_date = filters.get("from_date")
+	to_date = filters.get("to_date")
+	report_type = filters.get("report_type")
+	ref_practitioner = filters.get("ref_practitioner") if filters.get("ref_practitioner") is not None else ""
+	
+	columns =[
+		{"label": "Sales Invoice #", 'width': 150, "fieldname": "parent", "fieldtype":"Link", "options":"Sales Invoice"},
+		{"label": "Posting Date", 'width': 80, "fieldname": "posting_date"},
+		{"label": "Item", 'width': 80, "fieldname": "item_code"},
+		{"label": "PF Amount", 'width': 150, "fieldname": "amount", "fieldtype":"Currency", "precision":2},
+		{"label": "Amount to Turnover", 'width': 150, "fieldname": "amount_to_turnover", "fieldtype":"Currency", "precision":2},
+	]
+
+	data = get_data(from_date, to_date, report_type, ref_practitioner)
+
+	for row in data:
+		row['posting_date'] = frappe.db.get_value("Sales Invoice",row['parent'],'posting_date')
+
 	return columns, data
+
+def get_data(from_date, to_date, report_type, ref_practitioner):
+	return frappe.db.sql("""SELECT parent, item_code, amount, doctor, amount_to_turnover from `tabPF and Incentive Item` 
+					   			where docstatus = 1 and pf_type=%s and doctor like %s""",
+								(report_type,'%'+ref_practitioner+'%'), as_dict = True)
