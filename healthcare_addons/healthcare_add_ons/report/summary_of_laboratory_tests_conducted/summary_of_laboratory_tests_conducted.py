430dccad5772b622df0d0e96dff05e55d006766d
Herabelle Villanueva <villanuevahera@gmail.com>
2023-09-29 20:37:58 +0800

    Summary of Lab Tests and workspace changes

--- healthcare_addons/healthcare_add_ons/report/summary_of_laboratory_tests_conducted/summary_of_laboratory_tests_conducted.py
+++ healthcare_addons/healthcare_add_ons/report/summary_of_laboratory_tests_conducted/summary_of_laboratory_tests_conducted.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, littlehera and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 
 
 def execute(filters=None):
@@ -24,13 +24,20 @@ def get_data(from_date, to_date):
 	data = []
 	items = frappe.db.sql("""SELECT name, item_name from `tabItem` where item_group = 'Laboratory'""")
 	for item in items:
+		total_qty = 0
 		item_code = item[0]
 		item_name = item[1]
 		si_qty = get_si_item_total(from_date, to_date, item_code)
+		bundle_qty = get_bundle_item_total(from_date, to_date, item_code)
+		total_qty = si_qty + bundle_qty
+		if total_qty > 0 and not is_product_bundle(item_code):
+			data.append({"item_name":item_name,"qty":total_qty})
 	return data
 
 def get_si_item_total(from_date, to_date, item_code):
-	qty = frappe.db.sql("""SELECT SUM(qty) from `tabSales Invoice Item` where item_code = %s and docstatus = 1""",item_code)
+	qty = frappe.db.sql("""SELECT SUM(itm.qty) from `tabSales Invoice Item` itm join `tabSales Invoice` si on
+					 		si.name = itm.parent where itm.item_code = %s and si.docstatus = 1 and si.posting_date >=%s
+					 		and si.posting_date<=%s""",(item_code, from_date, to_date))
 	if len(qty)<1:
 		return 0
 	else:
@@ -38,8 +45,14 @@ def get_si_item_total(from_date, to_date, item_code):
 
 
 def get_bundle_item_total(from_date, to_date, item_code):
-	qty = frappe.db.sql("""SELECT SUM(qty) from `tabPacked Item` where item_code = %s and docstatus = 1""",item_code)
+	qty = frappe.db.sql("""SELECT SUM(itm.qty) from `tabPacked Item` itm join `tabSales Invoice` si on
+					 		si.name = itm.parent where itm.item_code = %s and si.docstatus = 1 and si.posting_date >=%s
+					 		and si.posting_date<=%s""",(item_code, from_date, to_date))
 	if len(qty)<1:
 		return 0
 	else:
 		return qty[0][0] if qty[0][0] is not None else 0
+
+def is_product_bundle(item_code):
+	count_item = frappe.db.sql("""SELECT count(*) from `tabProduct Bundle` where new_item_code = %s""",item_code)[0][0]
+	return True if (count_item is not None and count_item > 0) else False
