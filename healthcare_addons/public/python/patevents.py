c134ae147d8f356e43783cf26bcf9f32ddf118c7
Herabelle Villanueva <villanuevahera@gmail.com>
2023-10-05 14:51:26 +0800

    In Patient: follow lname, fname mi.  format

--- healthcare_addons/public/python/patevents.py
+++ healthcare_addons/public/python/patevents.py
@@ -3,8 +3,15 @@ import frappe, datetime
 def on_save(doc, method):
     lastname = doc.last_name
     firstname = doc.first_name
+    middlein = doc.middle_name[0] if (doc.middle_name is not None and doc.middle_name != "") else None
     birthdate = datetime.datetime.strptime(doc.dob,"%Y-%m-%d")
+    patient_name = lastname + ", "+ firstname
 
+    if middlein is not None:
+        patient_name = patient_name + " " + middlein+ "."
+    
+    doc.patient_name = patient_name
+ 
     doc.patient_id = lastname[0]+firstname[0]+str(birthdate.month).zfill(2)+str(birthdate.day).zfill(2)+str(birthdate.year)
     try:
         if doc.__islocal:
