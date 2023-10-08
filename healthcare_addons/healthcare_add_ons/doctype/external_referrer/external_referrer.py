2d07f7963dce33ace7c7951fec44cae2205f59d3
Herabelle Villanueva <villanuevahera@gmail.com>
2023-10-08 19:55:11 +0800

    Added salespartner to ext referrer

--- healthcare_addons/healthcare_add_ons/doctype/external_referrer/external_referrer.py
+++ healthcare_addons/healthcare_add_ons/doctype/external_referrer/external_referrer.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2023, littlehera and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
 class ExternalReferrer(Document):
-	pass
+	def validate(doc):
+		if find_sales_partner(doc.name):
+			doc.sales_partner = find_sales_partner(doc.name)
+		else:
+			sales_partner = {
+				"doctype": "Sales Partner",
+				"partner_name": doc.name,
+				"territory": "All Territories",
+				"commission_rate": 0
+			}
+
+			sp_doc = frappe.get_doc(sales_partner)
+			sp_doc.insert();
+			doc.sales_partner = sp_doc.name
+
+def find_sales_partner(sp_name):
+    return frappe.db.get_value("Sales Partner",sp_name,"partner_name")
