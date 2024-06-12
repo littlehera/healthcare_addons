3cd5ac2aab10e16c5230895a588dc5a268415e3f
Herabelle Villanueva <villanuevahera@gmail.com>
2024-06-12 13:02:31 +0800

    utils: Changed code for hmo fix to deduct consultation from total

--- healthcare_addons/utils.py
+++ healthcare_addons/utils.py
@@ -65,22 +65,27 @@ def referral_with_hmo():
         parent = row[0]
         doctor = row[3]
         pf_type = "Incentive"
-        custom_source = "Doctor's Referral- HMO"
         item_code = "INCENTIVE"
         parentfield = "custom_pf_and_incentives"
         parenttype = "Sales Invoice"
         idx =  get_idx(parent)
         sales_partner = get_sales_partner(doctor)
 
-        frappe.db.sql("""UPDATE `tabSales Invoice` set custom_source = %s, sales_partner =%s, amount_eligible_for_commission = net_total,
-                        commission_rate = 5, total_commission = (net_total * 0.05) where name = %s""",(custom_source, sales_partner, parent))
+        is_consultation, amount_to_deduct = check_if_consultation(parent)
+        if is_consultation:
+            amount -=amount_to_deduct
+        
+        if amount > 0:
 
-        if count_incentive_item(parent,doctor) == 0:
-            print("INSERT:", item_code, parent, doctor, idx, amount, name)
-            frappe.db.sql("""INSERT INTO `tabPF and Incentive Item`(name,creation,modified,modified_by,owner,docstatus,idx,item_code,amount,pf_type,
-                            doctor,external_referrer,amount_to_turnover,parent,parentfield,parenttype) VALUES(%s,NOW(),NOW(),'Administrator','Administrator',1,
-                            %s,%s,%s,%s,%s,NULL,%s,%s,%s,%s)""",(name,idx,item_code, amount, pf_type, doctor,amount, parent, parentfield, parenttype))
-        frappe.db.commit()
+            frappe.db.sql("""UPDATE `tabSales Invoice` set sales_partner =%s, amount_eligible_for_commission = net_total,
+                            commission_rate = 5, total_commission = (net_total * 0.05) where name = %s""",(sales_partner, parent))
+
+            if count_incentive_item(parent,doctor) == 0:
+                print("INSERT:", item_code, parent, doctor, idx, amount, name)
+                frappe.db.sql("""INSERT INTO `tabPF and Incentive Item`(name,creation,modified,modified_by,owner,docstatus,idx,item_code,amount,pf_type,
+                                doctor,external_referrer,amount_to_turnover,parent,parentfield,parenttype) VALUES(%s,NOW(),NOW(),'Administrator','Administrator',1,
+                                %s,%s,%s,%s,%s,NULL,%s,%s,%s,%s)""",(name,idx,item_code, amount, pf_type, doctor,amount, parent, parentfield, parenttype))
+            frappe.db.commit()
 
 def get_sales_partner(doctor):
     return frappe.db.get_value("Healthcare Practitioner",doctor,"custom_sales_partner")
@@ -88,3 +93,17 @@ def get_sales_partner(doctor):
 def count_incentive_item(parent, doctor):
     return frappe.db.sql("""SELECT COUNT(*) from `tabPF and Incentive Item` where parent = %s and item_code = 'INCENTIVE' and doctor = %s""",
                          (parent, doctor))[0][0]
+
+def check_if_consultation(si):
+    is_consultation = False
+    amount_to_deduct = 0
+
+    si_doc = frappe.get_doc("Sales Invoice", si)
+
+    items = si_doc.items
+    for item in items:
+        if "CONSULTATION" in str(item.item_code).upper():
+            is_consultation = True
+            amount_to_deduct += ((item.amount*.8) if "SC/PWD" in si_doc.custom_source else item.amount)
+    
+    return is_consultation, amount_to_deduct
