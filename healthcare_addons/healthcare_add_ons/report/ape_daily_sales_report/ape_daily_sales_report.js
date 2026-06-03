0f13e7840104a8879528895d0902b9ba92c6f2ef
Herabelle Villanueva <villanuevahera@gmail.com>
2026-06-03 09:11:43 +0800

    Added APE Daily Sales Report

---
+++ healthcare_addons/healthcare_add_ons/report/ape_daily_sales_report/ape_daily_sales_report.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2026, littlehera and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["APE Daily Sales Report"] = {
+	"filters": [
+		{
+            "fieldname": "report_type",
+            "label": __("Report Type"),
+            "fieldtype": "Select",
+            "options": ["With subtotals based on Source", "with Subtotal for each Form of Payment"],
+            "reqd": 1
+        },
+		{
+            "fieldname": "totals_only",
+            "label": __("Show Totals Only?"),
+            "fieldtype": "Check",
+            "default":0
+        }
+	],
+    "formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+        if(!(typeof data["custom_source"]=="undefined"))
+            if (data["custom_source"].includes('Total for')||data["custom_source"].includes('TOTAL')){
+                value = '<b style="background: #ffe9a5;">'+value+'</b>'
+        }
+        if(!(typeof data["payment_mode"]=="undefined"))
+            if (data["payment_mode"].includes('Total')||data["payment_mode"].includes('TOTAL')){
+                value = '<b style="background: #ffe9a5;">'+value+'</b>'
+            }
+		return value
+	}
+};
---
+++ healthcare_addons/healthcare_add_ons/report/ape_daily_sales_report/ape_daily_sales_report.py
@@ -0,0 +1,262 @@
+# Copyright (c) 2026, littlehera and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+import frappe, datetime
+
+
+def execute(filters=None):
+	columns, data = [], []
+
+	from_date = str(datetime.date.today())
+	to_date = str(datetime.date.today())
+	totals_only = filters.get("totals_only")
+	report_type = filters.get("report_type")
+	ape_type = "APE Only"
+
+	columns = get_columns(totals_only, report_type)
+	data = get_data(from_date, to_date, totals_only, report_type, ape_type, filters)
+	return columns, data
+
+def get_columns(totals_only, report_type):
+	#REPORT TYPES: "With subtotals based on Source", "with Subtotal for each Form of Payment","Summary based on Referred By"
+	if totals_only != 1:
+		if report_type in ["With subtotals based on Source"]:
+			columns = [
+				{"label": "Sales Invoice #", 'width': 150, "fieldname": "sales_invoice", "fieldtype":"Link", "options":"Sales Invoice"},
+				{"label": "Posting Date", 'width': 80, "fieldname": "posting_date"},
+				{"label": "Source", 'width': 150, "fieldname": "custom_source"},
+				{"label": "HMO", 'width': 150, "fieldname": "custom_hmo"},
+				{"label": "Patient", 'width': 150, "fieldname": "patient_name"},
+				{"label": "Referred By", 'width': 150, "fieldname": "custom_practitioner_name"},
+				{"label": "External Referrer", 'width': 150, "fieldname": "custom_external_referrer"},
+				{"label": "Gross Total", 'width': 150, "fieldname": "total", "fieldtype":"Currency", "precision":2},
+				{"label": "Discount %", 'width': 150, "fieldname": "additional_discount_percentage"},
+				{"label": "Discount Amount", 'width': 150, "fieldname": "discount_amount", "fieldtype":"Currency", "precision":2},
+				{"label": "Net Total", 'width': 150, "fieldname": "net_total", "fieldtype":"Currency", "precision":2},
+				{"label": "Form of Payment", 'width': 150, "fieldname": "payment_modes"}
+			]
+		else:
+			columns = [
+				{"label": "Form of Payment", 'width': 150, "fieldname": "payment_mode"},
+				{"label": "Sales Invoice #", 'width': 150, "fieldname": "sales_invoice", "fieldtype":"Link", "options":"Sales Invoice"},
+				{"label": "Posting Date", 'width': 80, "fieldname": "posting_date"},
+				{"label": "Source", 'width': 150, "fieldname": "custom_source"},
+				{"label": "Patient", 'width': 150, "fieldname": "patient_name"},
+				{"label": "Referred By", 'width': 150, "fieldname": "custom_practitioner_name"},
+				{"label": "External Referrer", 'width': 150, "fieldname": "custom_external_referrer"},
+				{"label": "Payment Amount", 'width': 150, "fieldname": "amount", "fieldtype":"Currency", "precision":2}
+			]
+	else:
+		if report_type == "With subtotals based on Source":
+			columns = [
+				{"label": "Source", 'width': 150, "fieldname": "custom_source"},
+				{"label": "Gross Total", 'width': 150, "fieldname": "total", "fieldtype":"Currency", "precision":2},
+				{"label": "Discount Amount", 'width': 150, "fieldname": "discount_amount", "fieldtype":"Currency", "precision":2},
+				{"label": "Net Total", 'width': 150, "fieldname": "net_total", "fieldtype":"Currency", "precision":2},
+			]
+		else:
+			columns = [
+				{"label": "Mode of Payment", 'width': 150, "fieldname": "payment_mode"},
+				{"label": "Amount", 'width': 150, "fieldname": "amount", "fieldtype":"Currency", "precision":2},
+			]
+
+	return columns
+
+def get_data(from_date, to_date, totals_only, report_type, ape_type, filters):
+	#REPORT TYPES: "With subtotals based on Source", "with Subtotal for each Form of Payment","Summary based on Referred By"
+	data = []
+	key_value = ""
+	if totals_only != 1:
+		if report_type == "With subtotals based on Source":
+			data = get_all_si(from_date, to_date, ape_type, filters)
+			if report_type == "With subtotals based on Source":
+				key_value = 'custom_source'
+			else:
+				key_value = 'custom_practitioner_name'
+
+		else:
+			key_value = 'payment_mode'
+			data = get_all_payments(from_date, to_date, ape_type, filters)
+
+		data = sorted(data, key=lambda k: k[key_value], reverse=False)
+		data = insert_subtotals(data, key_value)
+	else:
+		key_value = 'payment_mode' if report_type not in ["With subtotals based on Source","Summary based on Referred By"] else ''
+		data = get_totals_only(from_date, to_date, report_type, ape_type, filters)
+	
+	data = insert_total_row(data, key_value, totals_only)
+	
+	return data
+
+def get_all_si(from_date, to_date, ape_type, filters):
+	ref_practitioner = filters.get("referred_by") if filters.get("referred_by") is not None else ''
+	custom_source = filters.get("custom_source") if filters.get("custom_source") is not None else ''
+	payment_mode = filters.get("payment_mode") if filters.get("payment_mode") is not None else ''
+	data = []
+	
+	query = "SELECT DISTINCT si.* from `tabSales Invoice` si join `tabInvoice Payment Table` pmt on pmt.parent = si.name where si.posting_date >={} and si.posting_date<={} and si.docstatus = 1".format("'"+str(from_date)+"'", "'"+str(to_date)+"'")
+
+	if ref_practitioner != "":
+		query += " AND si.ref_practitioner = {}".format("'"+ref_practitioner+"'")
+	if custom_source != "":
+		query += " AND si.custom_source = {}".format("'"+custom_source+"'")
+	if payment_mode != "":
+		query += " AND pmt.payment_mode ={}".format("'"+payment_mode+"'")
+	if ape_type != "":
+		if ape_type == "APE Only":
+			query += " AND (si.custom_ape_type is not NULL AND si.custom_ape_type <>'')"
+		else:
+			query += " AND (si.custom_ape_type is NULL or si.custom_ape_type = '')"
+	
+	query += " order by si.name asc, posting_date asc"
+
+	print(query)
+
+	rows = frappe.db.sql(query, as_dict = True)
+
+	# rows = frappe.db.sql("""SELECT DISTINCT si.* from `tabSales Invoice` si join `tabInvoice Payment Table` pmt on pmt.parent = si.name 
+	# 				  where si.posting_date >=%s and si.posting_date<=%s and si.docstatus = 1 and si.ref_practitioner like %s
+	# 				  and si.custom_source like %s and pmt.payment_mode like %s order by si.name asc, posting_date asc""",
+	# 				  (from_date,to_date,'%'+ref_practitioner+'%','%'+custom_source+'%','%'+payment_mode+'%'), as_dict = True)
+
+	for row in rows:
+		row['sales_invoice'] = row['name']
+		row['payment_modes'] = get_payment_modes(row['name'])
+		if "HMO" in row['custom_source']:
+			row['custom_source']+= ' ('+get_hmo(row['name'])+')'
+		data.append(row)
+
+	return data
+
+def get_all_payments(from_date, to_date, ape_type, filters):
+	ref_practitioner = filters.get("referred_by") if filters.get("referred_by") is not None else ''
+	custom_source = filters.get("custom_source") if filters.get("custom_source") is not None else ''
+	payment_mode = filters.get("payment_mode") if filters.get("payment_mode") is not None else ''
+	data = []
+
+	print(ape_type)
+
+	query = "SELECT pmt.payment_mode, pmt.amount, pmt.ref_no, si.* from `tabSales Invoice` si join `tabInvoice Payment Table` pmt on pmt.parent = si.name where si.posting_date >={} and si.posting_date<={} and si.docstatus = 1".format("'"+str(from_date)+"'", "'"+str(to_date)+"'")
+
+	if ref_practitioner != "":
+		query += " AND si.ref_practitioner = {}".format("'"+ref_practitioner+"'")
+	if custom_source != "":
+		query += " AND si.custom_source = {}".format("'"+custom_source+"'")
+	if payment_mode != "":
+		query += " AND pmt.payment_mode ={}".format("'"+payment_mode+"'")
+	if ape_type != "":
+		if ape_type == "APE Only":
+			query += " AND (si.custom_ape_type is not NULL AND si.custom_ape_type <>'')"
+		else:
+			query += " AND (si.custom_ape_type is NULL or si.custom_ape_type = '')"
+
+	rows = frappe.db.sql(query, as_dict = True)
+
+	# rows = frappe.db.sql("""SELECT pmt.payment_mode, pmt.amount, si.* from `tabSales Invoice` si join `tabInvoice Payment Table` pmt 
+	# 				  on pmt.parent = si.name where si.posting_date >=%s and si.posting_date<=%s and 
+	# 				  si.docstatus = 1 and si.ref_practitioner like %s and si.custom_source like %s and pmt.payment_mode like %s
+	# 				  order by si.name asc""",
+	# 				  (from_date,to_date,'%'+ref_practitioner+'%','%'+custom_source+'%','%'+payment_mode+'%'), as_dict = True)
+	for row in rows:
+		row['sales_invoice'] = row['name']
+		data.append(row)
+
+	return data
+
+def get_payment_modes(sales_invoice):
+	payment_methods = ""
+	rows = frappe.db.sql("""SELECT payment_mode from `tabInvoice Payment Table` where parent = %s""",sales_invoice)
+	for i, row in enumerate(rows):
+		payment_methods+=str(row[0])
+		if len(rows)!= i+1:
+			payment_methods+=', '
+	
+	print(payment_methods)
+	print("#####################")
+	
+	return payment_methods
+
+def get_totals_only(from_date, to_date, report_type, ape_type, filters):
+	ref_practitioner = filters.get("referred_by") if filters.get("referred_by") is not None else ''
+	custom_source = filters.get("custom_source") if filters.get("custom_source") is not None else ''
+	payment_mode = filters.get("payment_mode") if filters.get("payment_mode") is not None else ''
+
+	data = []
+	
+	#REPORT TYPES: "With subtotals based on Source", "with Subtotal for each Form of Payment","Summary based on Referred By"
+	if report_type == "With subtotals based on Source":
+		data = frappe.db.sql("""SELECT si.custom_source, sum(si.total) as total, sum(si.discount_amount) as discount_amount, sum(si.net_total) 
+			   					as net_total from `tabSales Invoice` si where si.posting_date >=%s and si.posting_date<=%s and 
+					  			si.docstatus = 1 and si.ref_practitioner like %s and si.custom_source like %s 
+					   			AND (si.custom_ape_type is not NULL AND si.custom_ape_type <>'')
+					   			and name in (SELECT parent from `tabInvoice Payment Table` pmt where pmt.payment_mode like %s)
+					   			group by si.custom_source""",
+					  			(from_date,to_date,'%'+ref_practitioner+'%','%'+custom_source+'%','%'+payment_mode+'%'), as_dict = True)
+	else:
+		data = frappe.db.sql("""SELECT pmt.payment_mode as payment_mode, sum(pmt.amount) as amount from `tabInvoice Payment Table` pmt join `tabSales Invoice` si 
+					   			on si.name = pmt.parent	where si.posting_date >=%s and si.posting_date <=%s and si.docstatus = 1 
+					   			AND (si.custom_ape_type is not NULL AND si.custom_ape_type <>'')
+					   			and si.ref_practitioner like %s and si.custom_source like %s and pmt.payment_mode like %s
+					   			group by pmt.payment_mode""",
+								(from_date,to_date,'%'+ref_practitioner+'%','%'+custom_source+'%','%'+payment_mode+'%'), as_dict = True)
+
+	return data
+
+def insert_subtotals(data, key_name):
+	new_data = []
+
+	discount_total = 0
+	gross_total = 0
+	net_total = 0
+	total_amount = 0
+
+	prev_key_value = None
+	for row in data:
+		if (prev_key_value!= row[key_name]) or (prev_key_value is None):
+			if (prev_key_value is not None):
+				new_data.append({key_name:"Total for "+str(prev_key_value), "discount_amount":discount_total, "amount":total_amount, "total":gross_total, "net_total":net_total})
+				discount_total = 0
+				gross_total = 0
+				net_total = 0
+				total_amount = 0
+			prev_key_value = row[key_name]
+		
+		new_data.append(row)
+
+		discount_total += float(row['discount_amount'])
+		gross_total += float(row['total'])
+		net_total += float(row['net_total'])
+
+		if key_name == 'payment_mode':
+			total_amount += float(row['amount'])
+	
+	new_data.append({key_name:"Total for "+str(prev_key_value), "additional_discount_percentage":None, "discount_amount":discount_total, "total":gross_total, 
+					"net_total":net_total, "amount":total_amount})
+	return new_data
+
+
+def insert_total_row(data, key_value, totals_only):
+	discount_total = 0
+	gross_total = 0
+	net_total = 0
+	amount = 0 
+	for row in data:
+		if (totals_only !=1) and 'Total for' in row[key_value]:
+			continue
+		else:
+			if key_value == 'payment_mode':
+				amount += float(row['amount'])
+			else:
+				gross_total += float(row['total'])
+				net_total += float(row['net_total'])
+				discount_total += float(row['discount_amount'])
+
+	data.append({"custom_source":"TOTAL","payment_mode":"TOTAL", "additional_discount_percentage":None, "discount_amount":discount_total, "amount":amount, "total":gross_total, "net_total":net_total})
+	return data
+
+def get_hmo(si):
+	hmo = frappe.db.sql("""SELECT custom_hmo from `tabSales Invoice` where name = %s""", si)
+	return hmo[0][0] if hmo[0][0] is not None else ""
